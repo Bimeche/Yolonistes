17df12f3673c86ebb93c2d5266b3db85e15bf275
Yuu Nera <shilvatsuki@gmail.com>
2017-02-25 10:16:16 -0500

    Parser de base fini (2 fiches exemples)

--- Assets/Scripts/CharacterFile.cs
+++ Assets/Scripts/CharacterFile.cs
@@ -2,83 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class InfoFinancier
+{
+    public string dette;
+    public string infraction;
+
+    public InfoFinancier(string d, string inf)
+    {
+        dette = d;
+        infraction = inf;
+    }
+}
+
+public class InfoCriminel
+{
+    public string crime;
+
+    public InfoCriminel(string c)
+    {
+        crime = c;
+    }
+}
+
+public class InfoMedical
+{
+    public string psychologique;
+    public string condition;
+
+    public InfoMedical(string psy, string con)
+    {
+        psychologique = psy;
+        condition = con;
+    }
+}
+
+public class InfoPolitique
+{
+    public string religion;
+    public string engagement;
+
+    public InfoPolitique(string rel, string eng)
+    {
+        religion = rel;
+        engagement = eng;
+    }
+}
+
+public class CharacterInfo
+{
+    public bool guilty;
+    public string number;
+    public int age;
+    public string genre;
+    public string emploi;
+    public string marital;
+    public string hobbies;
+    public string notable;
+
+    public InfoCriminel crime;
+    public InfoFinancier finance;
+    public InfoMedical medical;
+    public InfoPolitique politique;
+
+    public CharacterInfo(bool g, string num, int a, string gen, string emp, string hob, string mar, string not, string d, string inf, string c, string psy, string con, string rel, string eng)
+    {
+        guilty = g;
+        number = num;
+        age = a;
+        genre = gen;
+        emploi = emp;
+        marital = mar;
+        hobbies = hob;
+        notable = not;
+
+        crime = new InfoCriminel(c);
+        finance = new InfoFinancier(d, inf);
+        medical = new InfoMedical(psy, con);
+        politique = new InfoPolitique(rel, eng);
+    }
+
+}
+
 public class CharacterFile : MonoBehaviour {
-    public class InfoFinancier
-    {
-        public string dette;
-        public string infraction;
+    
 
-        public InfoFinancier(string d, string inf)
-        {
-            dette = d;
-            infraction = inf;
-        }
-    }
-
-    public class InfoCriminel
-    {
-        public string crime;
-
-        public InfoCriminel(string c)
-        {
-            crime = c;
-        }
-    }
-
-    public class InfoMedical
-    {
-        public string psychologique;
-        public string condition;
-
-        public InfoMedical(string psy, string con)
-        {
-            psychologique = psy;
-            condition = con;
-        }
-    }
-
-    public class InfoPolitique
-    {
-        public string religion;
-        public string engagement;
-
-        public InfoPolitique(string rel, string eng)
-        {
-            religion = rel;
-            engagement = eng;
-        }
-    }
-
-    public class CharacterInfo
-    {
-        public string number;
-        public int age;
-        public string genre;
-        public string emploi;
-        public string hobbies;
-        public string notable;
-
-        public InfoCriminel crime;
-        public InfoFinancier finance;
-        public InfoMedical medical;
-        public InfoPolitique politique;
-
-        public CharacterInfo(string num, int a, string gen, string emp, string hob, string not, string d, string inf, string c, string psy, string con, string rel, string eng)
-        {
-            number = num;
-            age = a;
-            genre = gen;
-            emploi = emp;
-            hobbies = hob;
-            notable = not;
-
-            crime = new InfoCriminel(c);
-            finance = new InfoFinancier(d, inf);
-            medical = new InfoMedical(psy, con);
-            politique = new InfoPolitique(rel, eng);
-        }
-
-    }
+  
 
 	// Use this for initialization
 	void Start () {
--- Assets/Scripts/FileReader.cs
+++ Assets/Scripts/FileReader.cs
@@ -4,6 +4,7 @@ using System.IO;
 using UnityEngine;
 
 public class FileReader : MonoBehaviour {
+    public List<CharacterInfo> listCharacters = new List<CharacterInfo>();
 
     protected FileInfo theSourceFile = null;
     protected StreamReader reader = null;
@@ -11,17 +12,105 @@ public class FileReader : MonoBehaviour {
 
     void Start()
     {
-        theSourceFile = new FileInfo("Test.txt");
+        theSourceFile = new FileInfo("Fiches.txt");
         reader = theSourceFile.OpenText();
+
+        text = reader.ReadLine();
+
+        while (text != null)
+        {
+            CreateCharacter();
+            text = reader.ReadLine();
+        }
+
+        foreach(CharacterInfo i in listCharacters)
+        {
+            Debug.Log(i.guilty + " " + i.number + " " + i.age + " " + i.genre + " " + i.emploi + " " + i.hobbies + " " + i.notable + " " + i.finance.dette + " " + i.finance.infraction
+                + " " + i.crime.crime + " " + i.medical.psychologique + " " + i.medical.condition + " " + i.politique.religion + " " + i.politique.engagement +"\n");
+        }
+    }
+
+    public void CreateCharacter()
+    {
+        bool guilt = false;
+        string num;
+        int a;
+        string gen;
+        string emp;
+        string mar;
+        string hob;
+        string not = "";
+        string d = "";
+        string inf = "";
+        string c = "";
+        string psy = "";
+        string con = "";
+        string rel = " ";
+        string eng = "";
+
+        if (text.Equals("g"))
+        {
+            guilt = true;
+        }
+        text = reader.ReadLine();
+        num = text;
+        text = reader.ReadLine();
+        int.TryParse(text, out a);
+        text = reader.ReadLine();
+        gen = text;
+        text = reader.ReadLine();
+        emp = text;
+        text = reader.ReadLine();
+        mar = text;
+        text = reader.ReadLine();
+        hob = text;
+        text = reader.ReadLine();
+        if(!text.Equals("0"))
+        {
+            not = text;
+        }
+        text = reader.ReadLine();
+        if (!text.Equals("0"))
+        {
+            d = text;
+        }
+        text = reader.ReadLine();
+        if (!text.Equals("0"))
+        {
+            inf = text;
+        }
+        text = reader.ReadLine();
+        if (!text.Equals("0"))
+
+        {
+            c = text;
+        }
+        text = reader.ReadLine();
+        if (!text.Equals("0"))
+        {
+            psy = text;
+        }
+        text = reader.ReadLine();
+        if (!text.Equals("0"))
+        {
+            con = text;
+        }
+        text = reader.ReadLine();
+        if (!text.Equals("0"))
+        {
+            rel = text;
+        }
+        text = reader.ReadLine();
+        if (!text.Equals("0"))
+        {
+            eng = text;
+        }
+
+        listCharacters.Add(new CharacterInfo(guilt, num, a, gen, emp, mar, hob, not, d, inf, c, psy, con, rel, eng));
     }
 
     void Update()
     {
-        if (text != null)
-        {
-            text = reader.ReadLine();
-            //Console.WriteLine(text);
-            print(text);
-        }
+
     }
 }
