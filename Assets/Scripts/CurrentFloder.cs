584f9213dda24c0f14d44c7f255a57dab0e1532b
Yuu Nera <shilvatsuki@gmail.com>
2017-02-25 18:08:18 -0500

    ajout bad decisions

--- Assets/Scripts/CurrentFloder.cs
+++ Assets/Scripts/CurrentFloder.cs
@@ -7,9 +7,16 @@ public class CurrentFloder : MonoBehaviour {
     CharacterInfos current;
     int index = 1;
     public GameObject g;
+
+    public int badDecisions = 0;
+    public int numberFolder = 2;
+    public int innocentVictims = 0;
+
+    public int day;
     
 	// Use this for initialization
 	void Start () {
+        day = 1;
     }
     void Awake()
     {
@@ -31,6 +38,16 @@ public class CurrentFloder : MonoBehaviour {
     }
     public void NextFile()
     {
+        if(Files.listCharacters[index].outcome == 1 && Files.listCharacters[index].guilty == false)//judged guilty && actually non guilty
+        {
+            badDecisions++;
+            innocentVictims++;
+        }
+        else if(Files.listCharacters[index].outcome == 2 && Files.listCharacters[index].guilty == true)//judged innocent && actually guilty
+        {
+            badDecisions++;
+        }
+
         index++;
         Debug.Log("Count " + Files.listCharacters.Count + " index " +index);
         current = Files.listCharacters[index];
