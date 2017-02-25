faa1ad6d56a5aede8d1b5d3e6ceef5bdf9d3fac6
MorganeLeBris <morganebris@gmail.com>
2017-02-24 20:14:29 -0500

    office start

---
+++ Assets/Scenes/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Animator animator;                  //Used to store a reference to the Player's animator component.          
+    private Rigidbody2D rbody;
+    //Start overrides the Start function of MovingObject
+    protected void Start()
+    {
+        //Get a component reference to the Player's animator component
+        animator = GetComponent<Animator>();
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if(movement_vector != Vector2.zero)
+        {
+         //   animator.SetBool("iswalking", true);
+        }else
+        {
+           // animator.SetBool("iswalking", false);
+        }
+
+        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime * 5);
+    }
+}
