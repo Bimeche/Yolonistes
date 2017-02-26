23c50d3a308bf10923a357287c6eac70b8f328c8
Yuu Nera <shilvatsuki@gmail.com>
2017-02-25 23:53:44 -0500

    Joli menus Prep pour la fin (tout prêt sauf le contenu des trois scenes de fin elles-même : DeathEnding, GoodGuyEnding, et ThatWasCloseEnding)

--- Assets/Scripts/CurrentFloder.cs
+++ Assets/Scripts/CurrentFloder.cs
@@ -6,12 +6,13 @@ public class CurrentFloder : MonoBehaviour {
     public FileReader Files;
     CharacterInfos current;
 
-    int index = -1;
+    public int index = -1;
     public GameObject g;
 
     public int badDecisions = 0;
     public int numberFolder = 2;
     public int innocentVictims = 0;
+    public int guiltScore = 0;
 
     public int day;
     bool fileInitialized = false;
@@ -54,6 +55,7 @@ public class CurrentFloder : MonoBehaviour {
         else if(Files.listCharacters[index].outcome == 2 && Files.listCharacters[index].guilty == true)//judged innocent && actually guilty
         {
             badDecisions++;
+            guiltScore++; 
         }
 
         if (Files.listCharacters[index].outcome != 3)
@@ -61,13 +63,27 @@ public class CurrentFloder : MonoBehaviour {
             Files.listCharacters.RemoveAt(index);
         }
 
+        index = Random.Range(1, Files.listCharacters.Count);
+        Debug.Log("Count " + Files.listCharacters.Count + " index " + index);
+
         if (day >= 5 && numberFolder == 1)//si c'est le dernier jour et qu'il ne reste qu'un dossier, je tire le mien
         {
             index = 0;
+            //determine si joueur coupable
+            if(innocentVictims > 0)
+            {
+                Files.listCharacters[index].guilty = true;
+            }
+            else if(guiltScore > 4)
+            {
+                Files.listCharacters[index].guilty = true;
+            }
+            else
+            {
+                Files.listCharacters[index].guilty = false;
+            }
         }
 
-        index = Random.Range(1, Files.listCharacters.Count);
-        Debug.Log("Count " + Files.listCharacters.Count + " index " +index);
         current = Files.listCharacters[index];
     }
 
