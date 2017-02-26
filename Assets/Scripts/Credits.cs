fbe5ecc5feb377acf4bfc07cd6bc8e6034ffb709
Yuu Nera <shilvatsuki@gmail.com>
2017-02-26 15:07:21 -0500

    Credits incluent assets

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Credits : MonoBehaviour {
 	public AudioSource creditsMusic;
-	// Use this for initialization
-	void Start () {
+    public Text t1;
+    public Text t2;
+    public Text t3;
+    public Text texte;
+
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    public void ClickAssets()
+    {
+        t1.text = "";
+        t2.text = "";
+        t3.text = "";
+
+        texte.text = "Game Sprites adapted from: \n Pokemon HeartGold SoulSilver – DS, Pokemon Diamond Pearl – DS, Stardew Valley – PC, Army Men Advance – GBA, Papers, Please – PC, noirlac.tumblr.com \nMusic: \nTaylor Davis -Sadness and sorrow, Ofelia’s Dream";
+    }
 
     public void ClickBack()
     {
