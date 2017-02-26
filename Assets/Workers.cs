b17ad4517477c26c427580c519489de87b3cd2a6
MorganeLeBris <morganebris@gmail.com>
2017-02-26 10:11:07 -0500

    Killing team

---
+++ Assets/Workers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Workers : MonoBehaviour {
+
+    public int number;
+    CurrentFloder currentF;
+    // Use this for initialization
+    void Start () {
+        currentF = GameObject.Find("CurrentFolder").GetComponent<CurrentFloder>();
+        if (currentF.isDead(number))
+            gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
